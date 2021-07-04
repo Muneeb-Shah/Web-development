2ab19841e6aeb841bd5200556f01524961f6ba7e
Muneeb-Shah <amunib.kazmo@gmail.com>
2021-07-04 18:54:37 +0500

    psd html
    psd html

---
+++ web cit class tasks/psd to html task/ntechnology-html by muneeb/assets/js/menu.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    "use strict";
+    
+    $("#menu-icon").on("click", function () {
+        
+        $("#navbar ul").toggleClass("open");
+        
+    });
+    
+});
